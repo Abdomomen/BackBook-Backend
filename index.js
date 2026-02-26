6f3fa6f05c7824257f45ecc9bc3fa2a73a7f4f78
Abdomomen <omarmomen378@gmail.com>
2026-02-26 21:39:17 +0200

    Dockerize the backend for Back4app deployment

--- index.js
+++ index.js
@@ -79,8 +79,9 @@ app.use("/api/notifications", notificationRouter);
 app.use(errorHandler);
 if (require.main === module) {
   connectDB().then(() => {
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   });
 }
